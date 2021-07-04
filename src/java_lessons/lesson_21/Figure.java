806f81b3d496c0a8cf61d7a15b8a2548cee70d54
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-04 22:31:03 +0500

    L 21: add class Figure

---
+++ src/java_lessons/lesson_21/Figure.java
@@ -0,0 +1,19 @@
+package java_lessons.lesson_21;
+
+public abstract class Figure {
+    private final double[] sides;
+
+    protected Figure(double[] sides) {
+        this.sides = sides;
+    }
+
+    public double getPerimeter() {
+        double perimeter = 0;
+        for (double side: sides) {
+            perimeter += side;
+        }
+        return perimeter;
+    }
+
+    public abstract double getArea();
+}

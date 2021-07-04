58460d7681146a7efb352afc710112116be85d82
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-04 21:55:04 +0500

    L 21: add class Option

---
+++ src/java_lessons/lesson_21/Main.java
@@ -0,0 +1,10 @@
+package java_lessons.lesson_21;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Option op1 = new Option("ddd", true);
+
+    }
+}
---
+++ src/java_lessons/lesson_21/Option.java
@@ -0,0 +1,11 @@
+package java_lessons.lesson_21;
+
+public class Option {
+    private final String description;
+    private final boolean isChosen;
+
+    public Option(String description, boolean isChosen) {
+        this.description = description;
+        this.isChosen = isChosen;
+    }
+}

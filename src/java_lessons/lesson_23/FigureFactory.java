5440a1e21b4e63dd411487c7db6a5c0c6b5ecbe4
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-05 22:14:30 +0500

    L 23: replace if-else on switch in FigureFactory

--- src/java_lessons/lesson_23/FigureFactory.java
+++ src/java_lessons/lesson_23/FigureFactory.java
@@ -5,14 +5,12 @@ import java.util.Arrays;
 public class FigureFactory {
 
     public Figure create(double... sides) {
-        if (sides.length == 1) {
-            return new Circle(sides[0]);
-        } else if (sides.length == 2) {
-            return new Rectangle(sides[0], sides[1]);
-        } else if (sides.length == 3) {
-            return new Triangle(sides[0], sides[1], sides[2]);
-        } else {
-            throw new IllegalArgumentException("Can't create object with arguments " + Arrays.toString(sides));
-        }
+        return switch (sides.length) {
+            case 1 -> new Circle(sides[0]);
+            case 2 -> new Rectangle(sides[0], sides[1]);
+            case 3 -> new Triangle(sides[0], sides[1], sides[2]);
+            default -> throw new IllegalArgumentException("Can't create object with arguments "
+                    + Arrays.toString(sides));
+        };
     }
 }

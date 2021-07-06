cb51c35069bca3d0e9d288305924f578d85e2a5b
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-06 21:44:17 +0500

    L 25: copy Main from L 24

---
+++ src/java_lessons/lesson_25/Main.java
@@ -0,0 +1,18 @@
+package java_lessons.lesson_25;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static void print(String text) {
+        System.out.println(text);
+    }
+
+    private static void print(int text) {
+        System.out.println(text);
+    }
+}

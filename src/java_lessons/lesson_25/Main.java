e3763ca88f9f34fd3827389fc0ae86c5d5466977
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-07 00:40:48 +0500

    L 25: rename CloudDataSource to MyDataCloudDataSource

--- src/java_lessons/lesson_25/Main.java
+++ src/java_lessons/lesson_25/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DataSource<MyData> repository = new Repository<MyData>(
-                new CloudDataSource(),
+        DataSource<MyData> repository = new Repository<>(
+                new MyDataCloudDataSource(),
                 new CachedDataSource<>()
-                );
+        );
 
         MyData data = repository.getData();
         data = repository.getData();
--- src/java_lessons/lesson_25/MyDataCloudDataSource.java
+++ src/java_lessons/lesson_25/MyDataCloudDataSource.java
@@ -3,7 +3,7 @@ package java_lessons.lesson_25;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class CloudDataSource implements DataSource<MyData> {
+public class MyDataCloudDataSource implements DataSource<MyData> {
 
     @Nullable
     @Override

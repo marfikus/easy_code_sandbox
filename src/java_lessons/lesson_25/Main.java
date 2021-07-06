8445383f871a84f8a1a9dbbedea976917011400d
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-06 22:59:44 +0500

    L 25: add class Repository

--- src/java_lessons/lesson_25/Main.java
+++ src/java_lessons/lesson_25/Main.java
@@ -7,21 +7,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyData data = getMyData();
+        DataSource repository = new Repository(
+                new CachedDataSource(),
+                new CloudDataSource()
+        );
+
+        MyData data = repository.getData();
         print(data.toString());
 
     }
 
-    @NotNull
+/*    @NotNull
     private static MyData getMyData() {
         return new MyData(1, "dd");
-    }
+    }*/
 
-    private static void print(String text) {
+    private static void print(Object text) {
         System.out.println(text);
     }
 
-    private static void print(int text) {
-        System.out.println(text);
-    }
+//    private static void print(String text) {
+//        System.out.println(text);
+//    }
+
+//    private static void print(int text) {
+//        System.out.println(text);
+//    }
 }
---
+++ src/java_lessons/lesson_25/Repository.java
@@ -0,0 +1,25 @@
+package java_lessons.lesson_25;
+
+import org.jetbrains.annotations.Nullable;
+
+public class Repository extends DataSource {
+
+    private final DataSource cloudDataSource;
+    private final DataSource cachedDataSource;
+
+    public Repository(DataSource cloudDataSource,
+                      DataSource cachedDataSource) {
+        this.cloudDataSource = cloudDataSource;
+        this.cachedDataSource = cachedDataSource;
+    }
+
+    @Nullable
+    @Override
+    public MyData getData() {
+        MyData result = cachedDataSource.getData();
+        if (result == null) {
+            result = cloudDataSource.getData();
+        }
+        return result;
+    }
+}

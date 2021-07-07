73c48f8a2f23f61f6f7088b29070ed945619044b
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-07 14:31:53 +0500

    L 25 tasks: add workers and tasks in Main

--- src/java_lessons/lesson_25/tasks/Main.java
+++ src/java_lessons/lesson_25/tasks/Main.java
@@ -4,6 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Worker[] workers = {
+                new Designer("Jack"),
+                new Developer("Smith"),
+                new Tester("Max")
+        };
+
+        Task[] tasks = {
+                new DesignerTask("button_1 style",
+                        "we need style for the button_1...",
+                        false),
+                new DeveloperTask("button_1 code",
+                        "we need action on button_1 click...",
+                        false),
+                new TesterTask("button_1 test",
+                        "we need to check action on button_1 click...",
+                        false)
+        };
 
 
     }

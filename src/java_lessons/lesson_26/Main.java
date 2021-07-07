94a1fd33a9cb6fb4ec58657c600dca5cbd5f9cfe
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-07 18:28:45 +0500

    L 26: add class Task, copy Main from L 25

---
+++ src/java_lessons/lesson_26/Main.java
@@ -0,0 +1,14 @@
+package java_lessons.lesson_26;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static void print(Object text) {
+        System.out.println(text);
+    }
+}

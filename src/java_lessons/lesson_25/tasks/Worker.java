64fa3200c70d43811711e1a7358837143f8ea238
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-07 14:04:53 +0500

    L 25 tasks: add setTask in Task

--- src/java_lessons/lesson_25/tasks/Worker.java
+++ src/java_lessons/lesson_25/tasks/Worker.java
@@ -4,10 +4,17 @@ public abstract class Worker {
     private final String name;
     private final WorkerType type;
 
+    protected Task currentTask;
+
     protected Worker(String name, WorkerType type) {
         this.name = name;
         this.type = type;
     }
 
+    public void setTask(Task task) {
+        currentTask = task;
+    }
+
     public abstract void work();
+
 }

94c8cea9e033a3e8406ba66b280cac5d508d756d
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-08 23:18:48 +0500

    L 26: refactoring doTask in EmployeeChain

--- src/java_lessons/lesson_26/EmployeeChain.java
+++ src/java_lessons/lesson_26/EmployeeChain.java
@@ -10,15 +10,13 @@ public class EmployeeChain {
     }
 
     public boolean doTask(Task task) {
-        boolean result;
+        boolean result = false;
 
         if (employee.canHandleTask(task)) {
             employee.doTask(task);
             result = true;
         } else if (nextEmployeeChain != null) {
             result = nextEmployeeChain.doTask(task);
-        } else {
-            result = false;
         }
 
         return result;

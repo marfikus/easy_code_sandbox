b5600e3285bd0bc0f64999a9555f559cf2250f9f
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-08 22:19:30 +0500

    L 26: add logics in Main

--- src/java_lessons/lesson_26/Main.java
+++ src/java_lessons/lesson_26/Main.java
@@ -4,7 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        TaskFactory factory = new TaskFactory();
+        TaskProgressCallback callback = new TaskProgressCallback() {
+            @Override
+            public void updateTask(Task task) {
+                factory.updateTask(task);
+            }
+        };
 
+        EmployeeChain chain = new EmployeeChain(new Designer(callback, "Alice"));
+        EmployeeChain next = new EmployeeChain(new Programmer(callback, "Nick"));
+        EmployeeChain last = new EmployeeChain(new Tester(callback, "Jack"));
+        next.setNextEmployeeChain(last);
+        chain.setNextEmployeeChain(next);
+
+        while (true)
+            chain.doTask(factory.getTask());
 
     }
 

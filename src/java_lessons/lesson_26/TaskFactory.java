e070637aab696851fd40c8c3b0d74d3e55499b0c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-08 21:59:48 +0500

    L 26: add class TaskFactory

---
+++ src/java_lessons/lesson_26/TaskFactory.java
@@ -0,0 +1,43 @@
+package java_lessons.lesson_26;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TaskFactory {
+
+    private static final int SIZE = 10;
+    private final Task[] tasks;
+
+    public TaskFactory() {
+        tasks = new Task[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            tasks[i] = new Task(i, Task.Status.ASSEMBLING_REQUIREMENT,
+                    "description_" + i, "", "", "");
+        }
+    }
+
+    @NotNull
+    public Task getTask() {
+        Task result = null;
+        for (Task task : tasks) {
+            if (task.getStatus() != Task.Status.DONE) {
+                result = task;
+                break;
+            }
+        }
+
+        if (result == null) {
+            result = tasks[0];
+        }
+
+        return result;
+    }
+
+    public void updateTask(Task task) {
+        for (int i = 0; i < SIZE; i ++) {
+            if (tasks[i].getId() == task.getId()) {
+                tasks[i] = task;
+                break;
+            }
+        }
+    }
+}

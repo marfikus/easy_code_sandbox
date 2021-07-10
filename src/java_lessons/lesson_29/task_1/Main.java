ec1a476d578e63e882068c7b19953e96205081e2
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-10 21:35:38 +0500

    L 29 t 1: add class Triangle

---
+++ src/java_lessons/lesson_29/task_1/Main.java
@@ -0,0 +1,14 @@
+package java_lessons.lesson_29.task_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static void print(Object text) {
+        System.out.println(text);
+    }
+}
---
+++ src/java_lessons/lesson_29/task_1/Triangle.java
@@ -0,0 +1,15 @@
+package java_lessons.lesson_29.task_1;
+
+public class Triangle {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+
+}

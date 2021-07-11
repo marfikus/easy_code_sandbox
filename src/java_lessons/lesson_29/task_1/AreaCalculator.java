20527b24cb1a14a07a193162ae1a71de5ea247d1
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-11 13:56:30 +0500

    L 29 t 1: add getTriangleType in AreaCalculator

--- src/java_lessons/lesson_29/task_1/AreaCalculator.java
+++ src/java_lessons/lesson_29/task_1/AreaCalculator.java
@@ -11,7 +11,30 @@ public abstract class AreaCalculator {
     }
 
     private TriangleType getTriangleType(Triangle triangle) {
-        // TODO: 11.07.2021 определяем тип треугольника
+        TriangleType result;
+        double sideA = triangle.getSideA();
+        double sideB = triangle.getSideB();
+        double sideC = triangle.getSideC();
+
+        if (sideA == sideB && sideA == sideC) {
+            result = TriangleType.EQUILITERAL;
+
+        } else if ((sideA == sideB && sideA != sideC)
+                || (sideA == sideC && sideA != sideB)
+                || (sideB == sideC && sideB != sideA)) {
+            result = TriangleType.ISOSCELES;
+
+        } else if ((Math.pow(sideA, 2) + Math.pow(sideB, 2) == Math.pow(sideC, 2))
+                    || (Math.pow(sideA, 2) + Math.pow(sideC, 2) == Math.pow(sideB, 2))
+                    || (Math.pow(sideB, 2) + Math.pow(sideC, 2) == Math.pow(sideA, 2))) {
+            result = TriangleType.RECTANGULAR;
+
+        } else {
+            throw new IllegalArgumentException("Can't determine type on triangle with sides: "
+                    + sideA + " " + sideB + " " + sideC);
+        }
+
+        return result;
     }
 
     public abstract double calcArea();
--- src/java_lessons/lesson_29/task_1/TriangleType.java
+++ src/java_lessons/lesson_29/task_1/TriangleType.java
@@ -1,7 +1,7 @@
 package java_lessons.lesson_29.task_1;
 
 public enum TriangleType {
-    EQUILITERAL,
-    ISOSCELES,
-    RECTANGULAR
+    EQUILITERAL,    // равносторонний
+    ISOSCELES,      // равнобедренный
+    RECTANGULAR     // прямоугольный
 }

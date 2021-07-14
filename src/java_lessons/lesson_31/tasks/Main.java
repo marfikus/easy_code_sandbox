6500f7acaf11131f780aa88825b4d9c9a8af7ebe
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-14 21:56:58 +0500

    L 31 tasks: add 4 sumultanious threads

--- src/java_lessons/lesson_31/tasks/Main.java
+++ src/java_lessons/lesson_31/tasks/Main.java
@@ -1,5 +1,7 @@
 package java_lessons.lesson_31.tasks;
 
+import java.util.Date;
+
 public class Main {
 
     private static final String URL = "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4";
@@ -7,15 +9,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        print("Starting at " + new Date());
-        Thread downloadThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new DownloadFile(URL, FILE_NAME).start(2);
-            }
-        });
-        downloadThread.start();
-//        print("Finishing at " + new Date());
+        print("Starting at " + new Date());
+
+        Thread[] threads = new Thread[4];
+
+        for (int i = 0; i < threads.length; i++) {
+            int number = i;
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    new DownloadFile(URL, "downloads/someVideo_" + number + ".mp4").start(2);
+                }
+            });
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+            print("Thread started");
+        }
 
     }
 

6615f82fbd037705b8441c3910a26c9c7c9de42b
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-15 14:20:31 +0500

    L 32: copy files from L 31

---
+++ src/java_lessons/lesson_32/DownloadFile.java
@@ -0,0 +1,37 @@
+package java_lessons.lesson_32;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+
+public class DownloadFile {
+
+    private final String url;
+    private final String fileName;
+
+    public DownloadFile(String url, String fileName) {
+        this.url = url;
+        this.fileName = fileName;
+    }
+
+    public void start(int attempts) {
+        System.out.println("Starting downloading file at " + new Date());
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+            System.out.println("Finishing downloading file at " + new Date());
+        } catch (IOException e) {
+            if (attempts > 1) {
+                start(--attempts);
+            } else {
+                System.out.println("Downloading file failed at " + new Date());
+            }
+        }
+    }
+}

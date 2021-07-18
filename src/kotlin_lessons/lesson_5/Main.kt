c2d7b92dde371c5a43845a0c6e844cb5af4be8a3
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-18 16:10:56 +0500

    K L 5: override methods equals and hashCode in MyItem

--- src/kotlin_lessons/lesson_5/Main.kt
+++ src/kotlin_lessons/lesson_5/Main.kt
@@ -5,9 +5,23 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        var str: String = "dffsfgg"
-        println(str.getLastIndex())
+/*        var str: String = "dffsfgg"
+        println(str.getLastIndex())*/
 
+        val list = mutableListOf(MyItem(1, "1"))
+        list.addItem(MyItem(1, "1"))
+
+//        val item = MyItem(1, "1")
+//        val list = mutableListOf(item)
+//        list.addItem(item)
+
+        print(list)
+
+    }
+
+    fun <T> MutableList<T>.addItem(item: T) {
+        if (!contains(item))
+            add(item)
     }
 
     fun String.getLastIndex(): Int = length - 1
--- src/kotlin_lessons/lesson_5/MyItem.java
+++ src/kotlin_lessons/lesson_5/MyItem.java
@@ -1,5 +1,7 @@
 package kotlin_lessons.lesson_5;
 
+import java.util.Objects;
+
 public class MyItem {
 
     private final int i;
@@ -9,6 +11,19 @@ public class MyItem {
     public MyItem(int i, String s) {
         this.i = i;
         this.s = s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyItem myItem = (MyItem) o;
+        return i == myItem.i && Objects.equals(s, myItem.s);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i, s);
     }
 
     @Override

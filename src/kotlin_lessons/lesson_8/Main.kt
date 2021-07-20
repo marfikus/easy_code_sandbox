3a56bf89292a0df8186bfe0218c12893d56340c5
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-20 17:16:15 +0500

    K L 8: add example of getter/setter in kotlin

--- src/kotlin_lessons/lesson_8/Main.kt
+++ src/kotlin_lessons/lesson_8/Main.kt
@@ -5,8 +5,25 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
 
+        val d = Data()
+        d.i = 5
+        d.show()
+        println(d.i)
 
+    }
 
+    class Data {
+
+        var i: Int = 0
+            set(value) {
+                if (value > 100)
+                    field = value
+            }
+            get() = field + 1
+
+        fun show() {
+            println(i)
+        }
     }
 
 }
